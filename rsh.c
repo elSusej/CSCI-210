72eec7dab6ed8260c76ad6760a4d238fe5fbd82c
Jesus <jesus.ledezmatuba@gmail.com>
2024-11-10 13:52:41 -0700

    non-spawn cmds are functional

--- rsh.c
+++ rsh.c
@@ -11,12 +11,17 @@ extern char **environ;
 
 char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
 
-int isAllowed(const char*cmd) {
-	// TODO
-	// return 1 if cmd is one of the allowed commands
-	// return 0 otherwise
-	
-	return 0;
+int isAllowed(const char* cmd) {
+    // TODO
+    // return 1 if cmd is one of the allowed commands
+    // return 0 otherwise
+    for (int i = 0; i < 12; i++) {
+        if ( strcmp(cmd,allowed[i]) == 0 ) {
+            return 1;
+        }
+    }
+    
+    return 0;
 }
 
 int main() {
@@ -25,21 +30,82 @@ int main() {
     // Add variables as needed
 
     char line[256];
+    char linecpy[256];
+    char* argv[20];
+    char* token;
 
     while (1) {
 
-	fprintf(stderr,"rsh>");
+    fprintf(stdout,"rsh> ");
 
-	if (fgets(line,256,stdin)==NULL) continue;
+    if (fgets(line,256,stdin)==NULL) continue;
 
-	if (strcmp(line,"\n")==0) continue;
+    if (strcmp(line,"\n")==0) continue;
 
-	line[strlen(line)-1]='\0';
+    line[strlen(line)-1]='\0';
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+    strcpy(linecpy, line);
+    token = strtok(linecpy, " ");
+    int i = 0;
+
+    while (token != NULL) {
+        argv[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+
+    argv[i] = NULL;
+
+    if (isAllowed(argv[0]) == 0) {
+        printf("NOT ALLOWED!\n");
+    }
+    // TODO
+    // Add code to spawn processes for the first 9 commands
+    // And add code to execute cd, exit, help command
+    
+    /*if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    if (strcmp(argv[0],"cp") == 0) {
+        
+    }
+    */
+    if (strcmp(argv[0],"cd") == 0) {
+        if ((argv[1] != NULL) && (argv[2] != NULL)) {
+            printf("-rsh: cd: too many arguments");
+        }
+        else {
+            chdir(argv[1]);
+        }
+    }
+    if (strcmp(argv[0],"exit") == 0) {
+        break;
+    }
+    if (strcmp(argv[0],"help") == 0) {
+        printf("The allowed commands are:\n");
+        for (int i = 0; i < 12; i++) {
+            printf("%s\n", allowed[i]);
+        }
+    }
+    // Use the example provided in myspawn.c
 
     }
     return 0;
